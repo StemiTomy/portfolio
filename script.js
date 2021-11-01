23a7083dd0b811c18fd398e891ec5d8c05a41200
Stelut Tomoiaga <stemitomy@gmail.com>
2021-11-01 20:22:53 +0100

    Subo los archivos necesarios para github pages
    Se trata de index.html, style.css y script.js, de momento muy simple.

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY>2800){
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    
+    // script para ir hacia arriba
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    // scirpt para escribir
+    var typed = new Typed(".typing", {
+        strings: ["Programador", "Diseñador"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Programador", "Diseñador"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // activar menu pulsando el icono de barritas
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
