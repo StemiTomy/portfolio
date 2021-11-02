6946f093eff8b1090d69c9ef0daf48f3911113ad
Stelut Tomoiaga <stemitomy@gmail.com>
2021-11-02 21:05:21 +0100

    Funcionalidad al form con mail.php

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject = ['subject'];
+$message= $_POST['message'];
+
+$to = "stemitomy@gmail.com";
+
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+
+$headers = "From: noreply@steluttomoiaga.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+
+//redirect
+header("Location:thankyou.html");
+?>
